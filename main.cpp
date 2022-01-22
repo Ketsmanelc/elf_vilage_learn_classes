b58737c4bf130a2b80f86b268cc2b6d005e471c3
Ketsman Aleksandr <ketsman94@mail.ru>
2022-01-22 22:08:11 +0300

    add Tree classe

--- main.cpp
+++ main.cpp
@@ -21,7 +21,7 @@ public:
     }
 };
 
-class BigBranch //: public Branch
+class BigBranch
 {
     int count = 0;
     Branch** mediumBranch = nullptr;
@@ -43,6 +43,31 @@ public:
         }
     }
 
+    std::string getName ()
+    {
+
+        return this->ownerName;
+    }
+    int searchName(std::string searchName)
+    {
+        bool isName = false;
+        for (int i = 0; i < count; i++)
+        {
+            if (mediumBranch[i]->getName() == searchName) isName = true;
+        }
+        if (this->ownerName == searchName) isName = true;
+        if (isName)
+        {
+            int countNeighbour = 0;
+            for (int i = 0; i < count; i++)
+            {
+                if (mediumBranch[i]->getName() != "None") countNeighbour++;
+            }
+            countNeighbour += this->ownerName != "None";
+            return countNeighbour;
+        }
+    }
+
     int countNeighbor()
     {
         int amount = 0;
@@ -56,46 +81,51 @@ public:
 };
 
 
-#if 0
-class Tree: public BigBranch
+
+class Tree
 {
     int count = 0;
-    BigBranch** bigBranch= nullptr;
+    std::vector<BigBranch*> bigBranch{1};
 
 public:
     Tree (int inCount): count(inCount)
     {
         assert(inCount >= 0);
-        bigBranch = new BigBranch*[inCount];
+        //bigBranch = new BigBranch*[inCount];
+
         for (int i = 0; i < inCount; i++)
         {
-            std::string name;
-            std::cin >> name;
-            bigBranch[i] = new BigBranch(name);
+            int countMedium = 2 + rand() % (3 - 2);
+            bigBranch[i] = new BigBranch(countMedium);
+            bigBranch.push_back(bigBranch[i]);
         }
     }
-}
-public:
-#endif
+
+    int getCountNeigbour(std::string searchName)
+    {
+        for(int i = 0; i < count; i++)
+        {
+            int amount = bigBranch[i]->searchName(searchName) > 0;
+            if (amount)
+            {
+                std::cout << searchName << " has " <<  amount << " neighbour" ;
+                return amount;
+            }
+        }
+    }
+};
+
+
 
 int main() {
     int countBig = 3 + rand() % (5 - 3);
-    BigBranch** bigBranch = new BigBranch*[countBig];
-    for (int i = 0; i < countBig; i++)
-    {
-        int countMedium = 2 + rand() % (3 - 2);
-        bigBranch[i] = new BigBranch(countMedium);
-    }
+    //BigBranch** bigBranch = new BigBranch*[countBig];
+    Tree* tree = new Tree(countBig);
 
     std::cout << "Enter search name: ";
     std::string searchName;
     std::cin >> searchName;
-    for (int i = 0; i < countBig; i++)
-    {
-        if (searchName == bigBranch[i]. )
 
-            bigBranch[i]->countNeighbor();
-    }
-
+    tree->getCountNeigbour(searchName);
     return 0;
 }
